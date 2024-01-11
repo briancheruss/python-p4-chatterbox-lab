f6b152d8b53c786fa6b7b15a5d14893235408c89
briancherus <brian.cherus@student.moringaschool.comssh-keygen -t rsa -b 4096 -C brian.cherus@student.moringaschool.com>
2024-01-11 22:43:48 +0300

    completed assignment

--- server/app.py
+++ server/app.py
@@ -7,20 +7,49 @@ from models import db, Message
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
 
 CORS(app)
 migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
-def messages():
-    return ''
+# GET route to fetch all messages
+@app.route('/messages', methods=['GET'])
+def get_messages():
+    messages = Message.query.order_by(Message.created_at.asc()).all()
+    serialized_messages = [message.to_dict() for message in messages]
+    return jsonify(serialized_messages)
 
-@app.route('/messages/<int:id>')
-def messages_by_id(id):
-    return ''
+# POST route to create a new message
+@app.route('/messages', methods=['POST'])
+def create_message():
+    data = request.get_json()
+    new_message = Message(body=data['body'], username=data['username'])
+    db.session.add(new_message)
+    db.session.commit()
+    return jsonify(new_message.to_dict())
+
+# PATCH route to update a message by ID
+@app.route('/messages/<int:id>', methods=['PATCH'])
+def update_message(id):
+    data = request.get_json()
+    message = Message.query.get(id)
+    if message:
+        message.body = data.get('body', message.body)
+        db.session.commit()
+        return jsonify(message.to_dict())
+    else:
+        return jsonify({"error": "Message not found"}), 404
+
+@app.route('/messages/<int:id>', methods=['DELETE'])
+def delete_message(id):
+    message = Message.query.get(id)
+    if message:
+        db.session.delete(message)
+        db.session.commit()
+        return jsonify({"message": "Message deleted successfully"})
+    else:
+        return jsonify({"error": "Message not found"}), 404
 
 if __name__ == '__main__':
     app.run(port=5555)
